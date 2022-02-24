70e5333e7dd8ace4dbb2a216dfdc5a90e788c277
Mathias Ebbensgaard Jensen <maej@mir-robots.com>
2022-02-24 14:47:34 +0100

    Added more robots

--- label_data.py
+++ label_data.py
@@ -15,9 +15,16 @@ class labeldata:
         self.wifi = wifi_
 
 labels = [
-    labeldata("Gazellen", "gazelle.jpg", "MIR250", "Gazellen", "10.52.180.105", "MIR_204703014"),
-    labeldata("Pumba", "pumba.jpg", "MIR600", "Pumba", "10.52.180.180", "?"),
+    labeldata("Unnamed", "unnamed.jpg", "MIR250", "PS-004", "10.52.180.100", "PS-004"),
+#    labeldata("Unnamed", "unnamed.jpg", "MIR600", "Red Dragon Proto 1", "10.52.180.103", "?"),
     labeldata("Malik", "malik.jpg", "MIR100", "Malik - MIR100", "10.52.180.104", "?"),
-    labeldata("Rhino", "rhino.jpg", "MIR1350", "EMB 1350CC", "10.52.180.183", "?")
+    labeldata("Gazellen", "gazelle.jpg", "MIR250", "Gazellen", "10.52.180.105", "MIR_204703014"),
+    labeldata("Unnamed", "unnamed.jpg", "MIR200", "MiR_S1184", "10.52.180.107", "MIR_S1184"),
+    labeldata("Unnamed", "unnamed.jpg", "MIR500", "MiR_U0156", "10.52.180.179", "?"),
+    labeldata("Pumba", "pumba.jpg", "MIR600", "Pumba", "10.52.180.180", "?"),
+    labeldata("Nora", "nora.jpg", "MIR250", "PS16 - Nora", "10.52.180.181", "PS-016"),
+    labeldata("Yeti", "yeti.jpg", "MIR600", "Yeti - KCH", "10.52.180.182", "?"),
+    labeldata("Rhino", "rhino.jpg", "MIR1350", "EMB 1350CC", "10.52.180.183", "?"),
+    labeldata("Unnamed", "unnamed.jpg", "MIR200", "MiR_S1033", "?", "MiR_AGV7")
 ]
 
